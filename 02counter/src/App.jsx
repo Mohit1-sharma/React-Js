a178f328f6d368a2d10117a2c8fec2943ab1bbe2
Mohit1-sharma <ms0904339@gmail.com>
2025-07-31 23:04:57 +0530

    project counter made

--- 02counter/src/App.jsx
+++ 02counter/src/App.jsx
@@ -4,13 +4,37 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  let [counter, setCounter] = useState(15)
+  let [counter, setCounter] = useState(10)
   //let counter = 15
   const addValue = ()=>{
-    counter = counter + 1;
-    setCounter(counter)
+    ;
+    if(counter < 20){
+      counter = counter + 1
+      setCounter(counter)
+    } else{
+      alert('adding limit reached')
+      return false;
+    }
+    
+
+    
     
   }
+
+  const decreaseValue = ()=>{
+    if(counter > 0){
+       counter = counter -1
+    setCounter(counter)
+    } else{
+      alert('decreasing limit reached')
+      return false
+    }
+   
+   
+   
+  }
+  //  this is use case of use state, basically this is a container which take two parameter {counter, setcounter} =>
+    // counter hold the value and setcounter set the value by using same varibale we can define logic and use the setcounter to implent chnages in ui
   return (
     <>
       <h1>Chai aur React</h1>
@@ -18,7 +42,7 @@ function App() {
 
       <button onClick = {addValue}>Add value</button>
       <br></br>
-      <button>Decrease Value</button>
+      <button onClick = {decreaseValue}>Decrease Value</button>
 
     </>
   )
