b28478effe3629894708c186767bb06caccd1316
Mohit1-sharma <ms0904339@gmail.com>
2025-08-23 10:29:41 +0530

    made new react bgchanger project

--- 04bgChanger/src/App.jsx
+++ 04bgChanger/src/App.jsx
@@ -1,13 +1,34 @@
 import { useState } from 'react'
 
 function App() {
-  const [color, setcolor] = useState("olive");
+  const [color, setcolor] = useState("orange");
 
   return (
     <>
       <div className='w-full h-screen duration-200' style = {{ backgroundColor: color }}>
         <div className='fixed flex justify-center bottom-12 inset-x-0 px-2'>
-          test
+          <div className = 'flex flex-wrap justify-center gap-3 shadow-lg bg-white px-3 py-2 rounded-full'>
+            <button onClick = {() => setcolor('red')}
+            className = 'outline-none px-4 py-1 rounded-full text-white shadow-lg'
+            style = {{backgroundColor : 'red'}}
+            >RED</button>
+            <button onClick = {() => setcolor('blue')}
+            className = 'outline-none px-4 py-1 rounded-full text-white shadow-lg'
+            style = {{backgroundColor : 'blue'}}
+            >BLUE</button>
+            <button onClick = {() => setcolor('black')}
+            className = 'outline-none px-4 py-1 rounded-full text-white shadow-lg'
+            style = {{backgroundColor : 'black'}}
+            >BLACK</button>
+            <button onClick = {() => setcolor('green')}
+            className = 'outline-none px-4 py-1 rounded-full text-white shadow-lg'
+            style = {{backgroundColor : 'green'}}
+            >GREEN</button>
+            <button onClick = {() => setcolor('pink')}
+            className = 'outline-none px-4 py-1 rounded-full text-white shadow-lg'
+            style = {{backgroundColor : 'pink'}}
+            >PINK</button>
+          </div>
         </div>
 
       </div>
